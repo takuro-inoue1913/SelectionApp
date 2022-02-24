e43584f619675f763210b8176daa40cb75454102
takuro-inoue <takuro.inoue@blueish.co.jp>
2022-02-24 20:56:18 +0900

    セミコロンを不要にした

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from "react"
 
 function App() {
-  return <div className="App">test-app</div>;
+  return <div className="App">test-app</div>
 }
 
-export default App;
+export default App
