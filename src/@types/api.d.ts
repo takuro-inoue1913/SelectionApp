af4b5748d15236bd9ddd7509f5415e0989e43203
takuro-inoue <takuro.inoue@blueish.co.jp>
2022-02-27 00:47:12 +0900

    総人口取得のAPIをaxiosに変更

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -6,4 +6,22 @@ declare namespace ResasApi {
       prefName: string
     }[],
   }
+
+  export type GetPopulationCompositionResponse = {
+    message: string | null,
+    result: {
+      boundaryYear: number,
+      data: {
+        label: "総人口" | "年少人口" | "生産年齢人口" | "老年人口"
+        data: PopulationComposition.TotalPopulation[]
+      }[]
+    }
+  }
+}
+
+declare namespace PopulationComposition {
+    export type TotalPopulation = {
+      year: number,
+      value: number,
+  }
 }
--- src/api/resas.ts
+++ src/api/resas.ts
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import axios from './axios'
 
+// 値が長期間変化しないものはcreateApiを使用します
 export const resasApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: 'https://opendata.resas-portal.go.jp/api/v1/',
     prepareHeaders: (headers) => {
@@ -9,8 +11,17 @@ export const resasApi = createApi({
   endpoints: (builder) => ({
     getPrefectures: builder.query<ResasApi.GetPrefecturesResponse, void>({
       query: () => `prefectures`,
-    }),
+    })
   }),
 })
 
 export const { useGetPrefecturesQuery } = resasApi
+
+// 頻繁にリクエストするAPIに関してはaxiosを使用します
+export const getPopulationComposition = async (prefCode: number) => 
+  axios.get<ResasApi.GetPopulationCompositionResponse>(`/population/composition/perYear`, { params: { prefCode }, headers: { 'X-API-KEY': process.env.REACT_APP_RESAS_API_KEY as string } })
+  .then(res => {
+    const result = res.data.result.data.filter(d => d.label === '総人口')
+
+    return result[0].data
+  })
